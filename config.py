f71bbb97e27e20f334caf1693fdf67b65b2f3e53
mhijazi1 <mhijazi1@student.gsu.edu>
2015-11-02 04:20:39 -0500

    added config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+#Use this to configure tests, for now all that was needed was to setup the type of browser
+#(FF, Chrome, IE), please note that to use these you must download the appropriate driver
+# however firefox is already included in selenium by default
+#I will include a link below to each driver. Also please not that the IE driver is windows only
+
+#browser = "Chrome" # https://sites.google.com/a/chromium.org/chromedriver/home
+
+#browser = "IE" # https://github.com/SeleniumHQ/selenium/wiki/InternetExplorerDriver
+
+browser = "Firefox"
